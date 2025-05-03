374aff24881c49931a02b3607f0645ae79f76195
virakkhun <virak.dev.2000@gmail.com>
2025-05-03 17:06:14 +0700

    chore: fixed meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,10 +47,7 @@
 		name="description"
 		content="a Software Engineer based in Phnom Penh, graduated from RUPP majored in Computer Science"
 	/>
-	<meta
-		name="keywords"
-		content="portfolio,personal website,virakkhun,khunvirak,khun virak,virak dev,software engineer"
-	/>
+	<meta name="keywords" content="virakkhun,khunvirak,khun virak,virak dev" />
 	<meta name="referrer" content="no-referrer" />
 	<meta name="theme-color" content="#1A1A1A" />
 	<meta name="color-scheme" content="dark" />
@@ -61,31 +58,45 @@
 		property="og:description"
 		content="a Software Engineer based in Phnom Penh, graduated from RUPP majored in Computer Science"
 	/>
-	<meta property="og:url" content="https://virak.me/" />
+	<meta property="og:url" content="https://rakdotdev.space" />
 	<meta property="og:site_name" content="VIRAK KHUN" />
 	<meta property="og:locale" content="en-US" />
-	<meta property="og:image" content="https://avatars.githubusercontent.com/u/86554927?v=4" />
+	<meta
+		property="og:image"
+		content="https://media.licdn.com/dms/image/v2/D5603AQE6EDPn_HqMdw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1695958815370?e=2147483647&v=beta&t=9brfW4HANqoNceE_xNf4ebRtnLIcHPvZGrrDJdIuW74"
+	/>
+
 	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:url" content="https://rakdotdev.space" />
+	<meta name="twitter:domain" content="rakdotdev.space" />
 	<meta name="twitter:title" content="Virak Khun" />
+	<meta
+		property="twitter:description"
+		content="a Software Engineer based in Phnom Penh, graduated from RUPP majored in Computer Science"
+	/>
+	<meta
+		property="twitter:image"
+		content="https://media.licdn.com/dms/image/v2/D5603AQE6EDPn_HqMdw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1695958815370?e=2147483647&v=beta&t=9brfW4HANqoNceE_xNf4ebRtnLIcHPvZGrrDJdIuW74"
+	/>
+
+	<script type="application/ld+json">
+		{
+			"@context": "https://schema.org",
+			"@type": "Person",
+			"images": "https://media.licdn.com/dms/image/v2/D5603AQE6EDPn_HqMdw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1695958815370?e=2147483647&v=beta&t=9brfW4HANqoNceE_xNf4ebRtnLIcHPvZGrrDJdIuW74"
+			"name": "Virak Khun",
+      "email": "mailto:dev.virakkhun@gmail.com",
+			"url": "https://rakdotdev.space",
+			"description": "a Software Engineer based in Phnom Penh, graduated from RUPP majored in Computer Science",
+      "jobTitle": "Software Engineer"
+		}
+	</script>
 </svelte:head>
 
 <svelte:document on:keypress={onKeyPress} />
 
 <main class="container mx-auto md:px-24 px-4 relative">
 	<slot />
-
-	<script type="application/ld+json">
-		{
-			"@context": "https://schema.org",
-			"@type": "Person",
-			"name": "Virak Khun",
-			"url": "https://virak.vercel.app",
-			"image": [
-				"https://media.licdn.com/dms/image/v2/D5603AQE6EDPn_HqMdw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1695958815370?e=2147483647&v=beta&t=9brfW4HANqoNceE_xNf4ebRtnLIcHPvZGrrDJdIuW74"
-			],
-			"description": "a Software Engineer based in Phnom Penh, graduated from RUPP majored in Computer Science"
-		}
-	</script>
 </main>
 
 <KeymapStore keymaps={localEvent} />
