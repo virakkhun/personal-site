de6e5591493cf48ee9e62b1c3eb00730f05a9b97
virakkhun <virak.dev.2000@gmail.com>
2025-06-06 23:13:16 +0700

    chore: fixed(404 page)

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -10,6 +10,6 @@
 		<p class="text-2xl font-bold text-green-500">{$page.status}</p>
 		<p>--</p>
 		<p class="text-base font-bold">Page {$page.error.message}</p>
-		<a href="${base}/" class="rounded-md px-6 py-2 bg-blue-400 text-blue-50">back</a>
+		<a href="{base}/" class="rounded-md px-6 py-2 bg-blue-400 text-blue-50">back</a>
 	</div>
 {/if}
